cd66f9c2e34b7a530a6eb420e1db3689e0ed7283
Murutu <murutupeter@email.com>
2019-12-15 18:50:05 +0300

    Created setUp

--- test_credential.py
+++ test_credential.py
@@ -3,4 +3,15 @@ from credential import User
 from credential import Credential
 
 class TestUser(unittest.TestCase):
-    
+    '''
+    Test class that defines test cases for users
+
+    Args
+    unittest.TestCase to run before each test cases
+    '''
+
+    def setUp(self):
+        '''
+        Tear down method that does clean up after all test have been run
+        '''
+        User.user_list = [] 
