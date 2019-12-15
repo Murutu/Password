4f89788e7349dbf77d25f6aba4e723d722d7750b
Murutu <murutupeter@email.com>
2019-12-15 17:37:07 +0300

    Added user_list in credential.py

--- credential.py
+++ credential.py
@@ -1,3 +1,21 @@
 class User:
 
-    
+'''
+class that generates the users first name,last_name and password
+'''
+user_list = []
+
+def __init__(self,first_name,last_name,email,password):
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.password = password
+
+def save_user(self):
+    '''
+    save_user method saves user into user list
+    '''
+    User.user_list.append(self)
+
+@classmethod        
